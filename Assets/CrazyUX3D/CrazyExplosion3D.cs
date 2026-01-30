d44ae9afeb6020b9b9fcccc7a59a72577b37c2ed
otherpatrick <45902985+PatrickkikALT@users.noreply.github.com>
2026-01-30 14:18:50 +0100

    CrazyFixes3D

--- Assets/CrazyUX3D/CrazyExplosion3D.cs
+++ Assets/CrazyUX3D/CrazyExplosion3D.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections;
+using Unity.Netcode;
 using UnityEngine;
 
-public class CrazyExplosion3D : MonoBehaviour {
+public class CrazyExplosion3D : NetworkBehaviour {
   private Animator _animator;
   public GameObject player;
 
-  private void Start() {
+  public override void OnNetworkSpawn() {
     Destroy(gameObject, 0.9585f);
+  }
+
+  public override void OnDestroy() {
+    base.OnDestroy();
+    GetComponent<NetworkObject>().Despawn();
   }
 
   private void FixedUpdate() {
